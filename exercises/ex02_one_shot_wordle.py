02b95815b2b1e72f4438aed027d0eabf0e6d9561
Samantha Brinson <samilynn@unc.edu>
2023-09-15 16:43:53 -0400

    EX02 Progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,11 +3,16 @@
 __author__ = "730717721"
 
 secret_word: str = "python"
-secret_word_guess: str = input("What is your " + str(int(len(secret_word))) + "-letter guess? ")
+secret_word_guess: str = input("What is your " + str(len(secret_word)) + "-letter guess? ")
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
 while len(secret_word_guess) != len(secret_word):
-    incorrect_length: str = input("That was not " + str(int(len(secret_word))) + " letters! Try again: ")
-
+    incorrect_length: str = input("That was not " + str(len(secret_word)) + " letters! Try again: ")
+    if len(secret_word_guess) == len(secret_word):
+        print("Woo! You got it!")
+        
 if len(secret_word_guess) == len(secret_word) and secret_word_guess == secret_word:
     print("Woo! You got it!")
     exit()
