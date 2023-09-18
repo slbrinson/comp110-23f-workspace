ad8f853351b99b6dec529347774df6e1db501813
Samantha Brinson <samilynn@unc.edu>
2023-09-18 13:07:12 -0400

    EX02 PROGRESS

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,6 +6,7 @@ secret_word: str = "python"
 secret_word_guess: str = input(f"What is your {(len(secret_word))}-letter guess? ")
 
 letter_idx: int = 0
+emoji: str = ""
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -13,20 +14,19 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 while len(secret_word_guess) != len(secret_word):
     new_guess: str = input(f"That was not {(len(secret_word))} letters! Try again: ")
-    if len(new_guess) == len(secret_word) and new_guess != secret_word:
-        print("Not quite. Play again soon!")
-        exit()
-    if new_guess == secret_word:
-        print("Woo! You got it!")
-        exit()
+    secret_word_guess = new_guess
+    
+while letter_idx < len(secret_word):
+    if secret_word_guess[letter_idx] == secret_word[letter_idx]:
+        emoji = emoji + GREEN_BOX
+    else:
+        emoji = emoji + WHITE_BOX
+    letter_idx = letter_idx + 1
+
+print(emoji)
 
 if len(secret_word_guess) == len(secret_word) and secret_word_guess != secret_word:
     print("Not quite. Play again soon!")
-    exit()
 
-while letter_idx < len(secret_word):
-    if secret_word_guess or new_guess == secret_word:
-        print(GREEN_BOX)
-        letter_idx = letter_idx + 1
-        print("Woo! You got it!")
-        exit()
+if secret_word_guess or new_guess == secret_word:
+    print("Woo! You got it!")
