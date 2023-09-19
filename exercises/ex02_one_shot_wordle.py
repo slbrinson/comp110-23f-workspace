315186ecbd41c847a2539147b6e12cd665448fce
Samantha Brinson <samilynn@unc.edu>
2023-09-18 20:16:51 -0400

    ex02 wordle progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -13,20 +13,36 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(secret_word_guess) != len(secret_word):
+    #asking for another guess when the length of the guess is not the length of the secret word
     new_guess: str = input(f"That was not {(len(secret_word))} letters! Try again: ")
     secret_word_guess = new_guess
     
 while letter_idx < len(secret_word):
     if secret_word_guess[letter_idx] == secret_word[letter_idx]:
+        #if the index at each place of the words match, a green box will print
         emoji = emoji + GREEN_BOX
-    else:
+    if secret_word_guess[letter_idx] != secret_word[letter_idx]:
+        #if the indices do not match, a white box will print
         emoji = emoji + WHITE_BOX
+
+        #indices of the guess are in the secret word but are not in the same index position
+        letter_exists: secret_word_guess(letter_idx) == [secret_word] and not secret_word_guess[letter_idx] == secret_word[letter_idx]
+        alternate_idx: int = 0
+        while not(letter_exists) and alternate_idx < len(secret_word):
+            if alternate_idx == secret_word_guess[letter_idx]:
+                
+            else:
+                alternate_idx = alternate_idx + 1
+        if letter_exists == alternate_idx:
+            emoji = emoji + YELLOW_BOX
+
     letter_idx = letter_idx + 1
 
 print(emoji)
 
 if len(secret_word_guess) == len(secret_word) and secret_word_guess != secret_word:
+    #the lengths of the guess and secret word match but they are not the same word
     print("Not quite. Play again soon!")
 
-if secret_word_guess or new_guess == secret_word:
+if secret_word_guess == secret_word:
     print("Woo! You got it!")
