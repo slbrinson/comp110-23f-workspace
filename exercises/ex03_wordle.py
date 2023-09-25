847efb3159caec24935d182106f276a6e51172ec
Samantha Brinson <samilynn@unc.edu>
2023-09-25 08:47:22 -0400

    ex03 progress

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -18,3 +18,25 @@ def contains_char(word: str, single_char: str) -> bool:
 def emojified(guess: str, secret: str) -> str:
     """Will check indices and print corresponding colored emoji boxes."""
     assert len(guess) == len(secret)
+    letter_idx: int = 0
+    emoji: str = ""
+
+    while letter_idx < len(secret):
+        if contains_char(secret, guess[letter_idx]) is True and guess[letter_idx] == secret[letter_idx]:
+            emoji = emoji + GREEN_BOX
+        if contains_char(secret, guess[letter_idx]) is True and guess[letter_idx] != secret[letter_idx]:
+            emoji = emoji + YELLOW_BOX
+        if contains_char(secret, guess[letter_idx]) is False:
+            emoji = emoji + WHITE_BOX
+    letter_idx = letter_idx + 1
+    return emoji
+
+
+def input_guess(expected_len: int) -> str:
+    """Prompting for correct word length of secret word."""
+    guess: str = input(f"Enter a {expected_len} character word: ")
+
+    while len(guess) != expected_len:
+        if len(guess) != len(expected_len):
+            guess = input(f"That wasn't {expected_len} chars! Try again: ")
+        return input
