0d2af22ca919b13d8ea7f1ed039fd58e35d50c96
Samantha Brinson <samilynn@unc.edu>
2023-10-09 14:15:25 -0400

    Ex04 Progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,32 +2,39 @@
 
 __author__ = "730717721"
 
+
 def all(input: list[int], single_num: int) -> bool:
+    """Checking if all numbers in the list match a single given number."""
     all_idx: int = 0
     matches: bool = True
-
+    
     while all_idx < len(input):
-        if single_num == input[all_idx]:
+        if single_num == input[all_idx] and len(input) != 0:
             matches = True
         else:
             return False
         all_idx += 1
     return matches
         
+
 def max(input: list[int]) -> int:
+    """Finding the highest valued number in the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    list1_idx: int = 0
-    list2_idx: int = 0
+    """Checking if the numbers in each list match at each index."""
+    list_idx: int = 0
     matches: bool = True
 
-    while list1_idx and list2_idx < len(list1) and len(list2):
-        if list1[list1_idx] == list2[list2_idx]:
+    if len(list1) != len(list2):
+        return False
+
+    while list_idx < len(list1) and len(list2):
+        if (list1[list_idx] == list2[list_idx]):
             matches = True
-        elif list1[list1_idx] != list2[list2_idx]:
+        else:
             return False
-        list1_idx += 1
-        list2_idx += 1
+        list_idx += 1
     return matches
