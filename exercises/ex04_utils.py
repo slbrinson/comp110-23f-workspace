2c37a71d913ffc01420f61ee315bc65f2730bd01
Samantha Brinson <samilynn@unc.edu>
2023-10-09 11:47:09 -0400

    EX04 Progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,23 +4,30 @@ __author__ = "730717721"
 
 def all(input: list[int], single_num: int) -> bool:
     all_idx: int = 0
-    
-    while all_idx <= len(input) - 1:
-        if single_num == input[0]:
-            return True
+    matches: bool = True
+
+    while all_idx < len(input):
+        if single_num == input[all_idx]:
+            matches = True
+        else:
+            return False
         all_idx += 1
-        input[0] += 1
-    else:
-        return False
+    return matches
         
 def max(input: list[int]) -> int:
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    max_num = input[0]
-    while max_num < input[1]:
-        return input[1]
-    else:
-        return max_num
     
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    list1_idx: int = 0
+    list2_idx: int = 0
+    matches: bool = True
 
-def is_equal(list1: list[int] = list(), list2: list[int] = list()) -> bool:
+    while list1_idx and list2_idx < len(list1) and len(list2):
+        if list1[list1_idx] == list2[list2_idx]:
+            matches = True
+        elif list1[list1_idx] != list2[list2_idx]:
+            return False
+        list1_idx += 1
+        list2_idx += 1
+    return matches
