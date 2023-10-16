064f58c18c8f961fea35a825ce0e9795ff0bc72e
Samantha Brinson <samilynn@unc.edu>
2023-10-16 12:48:17 -0400

    CL06 progress

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,3 +2,10 @@
 
 __author__ = "730717721"
 
+def w_sum(vals: list[float]) -> float:
+    vals_idx: int = 0
+    vals_sum: float = 0.0
+    while vals_idx <= len(vals):
+        vals_sum += vals[vals_idx]
+        vals_idx += 1
+        return vals_sum
