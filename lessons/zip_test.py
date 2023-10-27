d520ccf0b1686a6d6253393c2c0e7d074b2f5c0e
Samantha Brinson <samilynn@unc.edu>
2023-10-27 12:18:42 -0400

    zip test complete

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -5,7 +5,25 @@ __author__ = "730717721"
 from lessons.zip import zip
 
 
-def test_zip_length() -> None:
-    """Testing on each list with one element each."""
-    test_list: list[str, int] = ["Hello", 5]
-    assert zip(test_list) == "Hello", 5
+def test_zip_list_length_one() -> None:
+    """Test dict with one key and one value."""
+    list1: list[str] = ["cupcake"]
+    list2: list[int] = [5]
+    test_list = {"cupcake": 5}
+    assert zip(list1, list2) == test_list
+
+
+def test_empty_zip() -> None:
+    """Testing if list is empty."""
+    list1: list[str] = []
+    list2: list[int] = []
+    test_list = {}
+    assert zip(list1, list2) == test_list
+
+
+def test_zip_list_length_two() -> None:
+    """Test dict with two keys and two values."""
+    list1: list[str] = ["cupcake", "pie"]
+    list2: list[int] = [5, 7]
+    test_list = {"cupcake": 5, "pie": 7}
+    assert zip(list1, list2) == test_list
