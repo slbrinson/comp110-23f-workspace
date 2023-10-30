ae0aff115430a3d5651486ea89d1805316f1161f
Samantha Brinson <samilynn@unc.edu>
2023-10-30 07:35:55 -0400

    ex06 progress

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -2,17 +2,17 @@
 
 __author__ = "730717721"
 
+
 def invert(input: dict[str, str]) -> dict[str, str]:
     """Inverting the keys and values."""
     invert_dict = dict()
     for keys in input:
-        if keys
+        value = input[keys]
+        if value in invert_dict:
             raise KeyError("Duplicate entry!")
-
+        invert_dict[value] = keys
     return invert_dict
 
 
 def favorite_colors(input: dict[str, str]) -> str:
     """Return the color that appears most."""
-
-
