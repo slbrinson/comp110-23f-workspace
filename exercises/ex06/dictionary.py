d01cdf5e3bac1cfb390c8f17770a92f7edaf5bf5
Samantha Brinson <samilynn@unc.edu>
2023-10-30 13:02:21 -0400

    ex06 progress

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -14,5 +14,30 @@ def invert(input: dict[str, str]) -> dict[str, str]:
     return invert_dict
 
 
-def favorite_colors(input: dict[str, str]) -> str:
+def favorite_color(input: dict[str, str]) -> str:
     """Return the color that appears most."""
+    colors_input = str()
+    colors_count: int = 0
+    popular_color = None
+    for name in input:
+        color = input[name]
+        if color in input:
+            colors_count += 1
+            color = colors_input
+        else:
+            colors_count = 1
+        if popular_color is None or (colors_count[color] > colors_count[popular_color]):
+            popular_color = color
+    return popular_color
+
+
+def count(input: list[str]) -> dict[str, int]:
+    """Counting number of times values appear in list."""
+
+
+def alphabetizer(input: list[str]) -> dict[str, list[str]]:
+    """Returns a letter and words that begin with the letter."""
+
+
+def update_attendance(days_and_students: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Return the students who were in attendance on certain days of the week."""
