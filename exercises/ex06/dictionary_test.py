9411ba3d4af8485a80664641503709268f65d7ae
Samantha Brinson <samilynn@unc.edu>
2023-11-08 10:09:20 -0500

    Finished

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -4,6 +4,7 @@ __author__ = "730717721"
 
 from exercises.ex06.dictionary import invert, favorite_color, count, alphabetizer, update_attendance
 
+
 # three tests for invert
 def test_empty_invert() -> None:
     """Testing if invert is empty."""
@@ -55,6 +56,7 @@ def test_empty_count() -> None:
     expected_count = {}
     assert count(input) == expected_count
 
+
 def test_count() -> None:
     """Testing if count is working for one letter."""
     input: list[str] = "p"
@@ -62,7 +64,7 @@ def test_count() -> None:
     assert count(input) == expected_count
 
 
-def test_empty_count_two_words() -> None:
+def test_empty_count_one_word() -> None:
     """Testing if count is working for a word."""
     input: list[str] = "pie"
     expected_count = {"p": 1, "i": 1, "e": 1}
@@ -70,6 +72,50 @@ def test_empty_count_two_words() -> None:
 
 
 # three tests for alphabetizer
+def test_empty_alphabetizer() -> None:
+    """Testing if alphabetizer is empty."""
+    input: list[str] = ""
+    expected_output = {}
+    assert alphabetizer(input) == expected_output
+
+
+def test_alphabetizer() -> None:
+    """Testing if alphabetizer is working with one word."""
+    input: list[str] = ["hello"]
+    expected_output = {"h": ["hello"]}
+    assert alphabetizer(input) == expected_output
+
+
+def test_alphabetizer_two_words() -> None:
+    """Testing if alphabetizer is working with multiple words."""
+    input: list[str] = ["hello", "dog", "happy", "dandelion"]
+    expected_output = {"h": ["hello", "happy"], "d": ["dog", "dandelion"]}
+    assert alphabetizer(input) == expected_output
 
 
 # three tests for update_attendance
+def test_update_attendance() -> None:
+    """Testing if update_attendance is working for multiple days."""
+    attendance_log: dict[str, list[str]] = {"Monday": "Sam"}
+    day: str = "Tuesday"
+    student: str = "Sam"
+    expected_attendance_log = {"Monday": "Sam", "Tuesday": ["Sam"]}
+    assert update_attendance(attendance_log, day, student) == expected_attendance_log
+
+
+def test_update_attendance_add_student() -> None:
+    """Testing if update_attendance is working for adding student."""
+    attendance_log: dict[str, list[str]] = {"Monday": "Sam"}
+    day: str = "Tuesday"
+    student: str = "Bailee"
+    expected_attendance_log = {"Monday": "Sam", "Tuesday": ["Bailee"]}
+    assert update_attendance(attendance_log, day, student) == expected_attendance_log
+
+
+def test_update_attendance_add_student_and_day() -> None:
+    """Testing if update_attendance is working for adding student and a day."""
+    attendance_log: dict[str, list[str]] = {"Monday": "Sam"}
+    day: str = "Tuesday"
+    student: str = "Sam", "Bailee"
+    expected_attendance_log = {"Monday": "Sam", "Tuesday": [("Sam", "Bailee")]}
+    assert update_attendance(attendance_log, day, student) == expected_attendance_log
