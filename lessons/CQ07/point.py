07dbabc114160e82f7c655dd679edd0e8b5a4c5d
Samantha Brinson <samilynn@unc.edu>
2023-11-12 11:36:46 -0500

    CQ07 Finished

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -7,10 +7,10 @@ __author__ = "730717721"
 
 class Point:
     """Defining Point class."""
-    x: float
-    y: float
+    x: float | int
+    y: float | int
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float | int = 0.0, y_init: float | int = 0.0):
         """Defining constructor."""
         self.x = x_init
         self.y = y_init
@@ -23,4 +23,19 @@ class Point:
     def scale(self, factor: int) -> Point:
         """Method that belongs to the Point class and creates a new Point."""
         new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __str__(self) -> str:
+        """Print out the points in a readable way."""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
+
+    def __mul__(self, factor: int | float) -> Point:
+        """Multiplication operator overload."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __add__(self, factor: int | float) -> Point:
+        """Addition operator overload."""
+        new_point: Point = Point(self.x + factor, self.y + factor)
         return new_point
